236bb1589d7ee218f64fc6f078588ddceffe2ce8
test <715858814@qq.com>
2023-06-21 08:39:15 +0800

    :tada:初始化提交

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import jQuery from 'jquery';
+import _ from 'lodash';
+import { a } from '@/script/app';
+import '@/style/index.less';
+import '@/index.css';
+
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+jQuery(() => {
+  console.log(123);
+});
+jQuery.ajax({
+  url: '/api/abc',
+});
+jQuery.ajax({
+  url: '/api1/abc1',
+});
+console.log(_);
+
+console.log('测试gitee -> github同步功能！');
+console.log('npm_package_config_port', npm_package_config_port);
+// 测试git revert
+// shenhongchun
+// 秃头程序员敲代码咯
+// 爱穿背带裤的小黑子喜欢敲代码
+const count = 123;
+// auto pr merge
+// 床前明月光
+// 家人们谁懂啊？🐖🐖
